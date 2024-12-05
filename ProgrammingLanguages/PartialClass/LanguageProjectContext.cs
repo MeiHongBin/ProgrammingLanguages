f158935ff175689841a07951f8501bb46497edde
steven <mistake123415@gmail.com>
2024-12-06 03:08:35 +0800

    新增擴充類別，目前僅有USER

---
+++ ProgrammingLanguages/PartialClass/LanguageProjectContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ProgrammingLanguages.Models
+//與源專案一樣，風險較小
+//OnConfiguring不會被自動生成覆蓋
+{
+    public partial class LanguageProjectContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+            IConfiguration config = new ConfigurationBuilder()
+            .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+            .AddJsonFile("appsettings.json")
+            .Build();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("LanguageProject"));
+            }
+        }
+    }
+}
