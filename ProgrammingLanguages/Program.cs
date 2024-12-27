d0b90394bd3962bf0e3f2ea97f2352ce2a22aa60
steven <mistake123415@gmail.com>
2024-12-27 21:09:31 +0800

    1.新增program註釋 2.新增middleware範例

--- ProgrammingLanguages/Program.cs
+++ ProgrammingLanguages/Program.cs
@@ -1,47 +1,64 @@
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.CodeAnalysis.Options;
 using Microsoft.EntityFrameworkCore;
 using ProgrammingLanguages.Data;
+using ProgrammingLanguages.middleware_test;
 using ProgrammingLanguages.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//連接字串定義
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
 var LanguageProjectconnectionString = 
     builder.Configuration.GetConnectionString("LanguageProject") ?? throw new InvalidOperationException("Connection string 'LanguageProject' not found.");
+//新增至DI容器內
+builder.Services.AddDbContext<ApplicationDbContext>(options =>options.UseSqlServer(connectionString));
 builder.Services.AddDbContext<LanguageProjectContext>(Options => Options.UseSqlServer(LanguageProjectconnectionString));
 
+//這會擷取可使用 Entity Framework 移轉解析的資料庫相關例外狀況。 發生這些例外狀況時，會產生 HTML 回應，其中包含可能解決問題的動作詳細資料。
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+//基礎身分驗證
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+//就是mvc模型生成必要程式
+//AddControllersWithViews可參考https://blog.csdn.net/WuLex/article/details/118919396
 builder.Services.AddControllersWithViews();
 
+//middleware中介軟體
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Configure the HTTP request pipeline.配置 HTTP 請求管道
+if (app.Environment.IsDevelopment())//環境是否為開發
 {
     app.UseMigrationsEndPoint();
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //錯誤時導向/Home/Error
     app.UseHsts();
+    //開啟 HSTS（HTTP Strict Transport Security），這是一種安全功能，強制瀏覽器以 HTTPS 通信，避免中間人（Man-in-the-Middle Attack, MITM）攻擊。
 }
 
-app.UseHttpsRedirection();//�s��http->�۰�https
-app.UseStaticFiles();//�R�A���s���Ƨ��A�w�]www.root
-app.UseRouting();//�ҥ�URL Routing
-app.UseAuthorization();//���v  �������ҫ�->���v
-//�i�ˤ����h���ҧ�h�F��app.UseMiddleware<>{ }
+app.UseHttpsRedirection();//瀏覽http->自動https
+app.UseStaticFiles();//靜態文件存放資料夾，預設www.root
+app.UseRouting();//啟用URL Routing
+app.UseAuthorization();//授權  身分驗證後->授權
+app.UseMiddleware<RequestTimingMiddleware>();//測試middleware用，位置:middleware_test\RequireRunning_time.cs
+//可裝中間層驗證更多東西app.UseMiddleware<>{ }
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+    pattern: "{controller=Home}/{action=Index}/{id?}"
+    //pattern: "Home/{id?}",
+    //defaults: new { controller = "Home", action = "Index" }
+    );
+//這一段是使用area的部份，原先Home/Index會變成admin/Home/Index，使用此網址呼叫時會導入其他網頁
+//app.MapControllerRoute(
+//    name: "area",
+//    pattern: "{area:admin}/{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();//會將 Razor Pages 的端點新增至 IEndpointRouteBuilder(將路由產生的意思)。
 
 app.Run();
---
+++ ProgrammingLanguages/middleware_test/RequireRunning_time.cs
@@ -0,0 +1,33 @@
+﻿namespace ProgrammingLanguages.middleware_test
+{
+    public class RequireRunning_time
+    {
+    }
+    public class RequestTimingMiddleware
+    {
+        //DI容器
+        //私人 唯讀 處理http請求
+        private readonly RequestDelegate _next;
+
+        public RequestTimingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var startTime = DateTime.UtcNow;
+
+            // 繼續執行後續的 Middleware 或處理請求
+            await _next(context);
+
+            var endTime = DateTime.UtcNow;
+            var duration = endTime - startTime;
+
+            //context.Request.Method 請求方法
+            //context.Request.Path 請求路徑
+            Console.WriteLine($"Request [{context.Request.Method}] {context.Request.Path} took {duration.TotalMilliseconds} ms");
+        }
+    }
+
+}
